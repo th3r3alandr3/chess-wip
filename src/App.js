4866dceba4e9fe6e89a9123e07e33ac12dd79207
Andre <andre@eidemann.de>
2023-03-16 22:58:21 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,207 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react'
+import {Canvas, useLoader, useThree} from '@react-three/fiber'
+import {OrbitControls, Environment, CameraControls} from '@react-three/drei'
+import * as THREE from "three";
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import Chess from "./Chess.ts";
+import {gsap} from "gsap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const chess = new Chess();
+const colorWhite = 0xff0000;
+const colorBlack = 0x00ff00;
+let activeColor = colorWhite;
+
+const App = () => {
+    const rookGLTF = useLoader(GLTFLoader, '/models/rook.glb');
+    const knightGLTF = useLoader(GLTFLoader, '/models/knight.glb');
+    const bishopGLTF = useLoader(GLTFLoader, '/models/bishop.glb');
+    const queenGLTF = useLoader(GLTFLoader, '/models/queen.glb');
+    const kingGLTF = useLoader(GLTFLoader, '/models/king.glb');
+    const pawnGLTF = useLoader(GLTFLoader, '/models/pawn.glb');
+
+    const rook = initGLTF(rookGLTF);
+    const knight = initGLTF(knightGLTF);
+    const bishop = initGLTF(bishopGLTF);
+    const queen = initGLTF(queenGLTF);
+    const king = initGLTF(kingGLTF);
+    const pawn = initGLTF(pawnGLTF);
+
+    const rookPositions = [[-3.5, 0, 3.5], [-3.5, 0, -3.5], [3.5, 0, 3.5], [3.5, 0, -3.5]]
+    const rooks = [];
+    rookPositions.forEach((position) => {
+        const clone = rook.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        rooks.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const knightPositions = [[-2.5, 0, 3.5], [-2.5, 0, -3.5], [2.5, 0, 3.5], [2.5, 0, -3.5]]
+    const knights = [];
+    knightPositions.forEach((position) => {
+        const clone = knight.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        knights.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const bishopPositions = [[-1.5, 0, 3.5], [-1.5, 0, -3.5], [1.5, 0, 3.5], [1.5, 0, -3.5]]
+    const bishops = [];
+    bishopPositions.forEach((position) => {
+        const clone = bishop.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        bishops.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const queenPositions = [[-.5, 0, 3.5], [-.5, 0, -3.5]]
+    const queens = [];
+    queenPositions.forEach((position) => {
+        const clone = queen.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        queens.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const kingPositions = [[.5, 0, 3.5], [.5, 0, -3.5]]
+    const kings = [];
+    kingPositions.forEach((position) => {
+        const clone = king.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        kings.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const pawnPositions = [[-3.5, 0, 2.5], [-3.5, 0, -2.5], [3.5, 0, 2.5], [3.5, 0, -2.5], [-2.5, 0, -2.5], [-2.5, 0, 2.5], [2.5, 0, -2.5], [2.5, 0, 2.5], [-1.5, 0, -2.5], [-1.5, 0, 2.5], [1.5, 0, -2.5], [1.5, 0, 2.5], [-.5, 0, -2.5], [-.5, 0, 2.5], [.5, 0, -2.5], [.5, 0, 2.5]];
+    const pawns = [];
+    pawnPositions.forEach((position) => {
+        const clone = pawn.clone();
+        clone.material = clone.material.clone();
+        clone.material.color = new THREE.Color(position[2] > 0 ? colorWhite : colorBlack);
+        pawns.push(<primitive object={clone} position={[position[0], position[1], position[2]]} onClick={pieceClick}/>);
+    });
+
+    const boardTexture = useLoader(THREE.TextureLoader, 'textures/board.jpg')
+
+    const controls = useRef();
+    const fields = [];
+    for (let i = 0; i < 8; i++) {
+        for (let j = 7; j >= 0; j--) {
+            const name = 'field-' + String.fromCharCode(65 + i) + (8 - j);
+            fields.push(
+                <mesh position={[i - 3.5, .026, j - 3.5]} rotation-x={-Math.PI / 2} name={name} onClick={(event) => fieldClick(event, controls)}>
+                    <planeGeometry args={[1, 1, 1]}/>
+                    <meshBasicMaterial color={colorBlack} transparent opacity={0}/>
+                </mesh>
+            );
+        }
+    }
+
+    return (
+        <div style={{
+            width: "100vw",
+            height: "100vh"
+        }}>
+            <Canvas camera={{
+                fov: 75,
+                near: 0.1,
+                far: 1000,
+                position: [0, 5, 6]
+            }}>
+                <ambientLight intensity={0.5}/>
+                <pointLight position={[-10, 0, -20]} color="white" intensity={1}/>
+                <pointLight position={[0, -10, 0]} intensity={1}/>
+                <Environment preset="studio"/>
+                <CameraControls ref={controls}/>
+                <mesh rotation-x={-Math.PI / 2}>
+                    <boxGeometry args={[8.63, 8.63, .05]}/>
+                    <meshBasicMaterial map={boardTexture}/>
+                </mesh>
+                {fields}
+                {rooks}
+                {knights}
+                {bishops}
+                {queens}
+                {kings}
+                {pawns}
+            </Canvas>
+        </div>
+    );
+}
+
+const initGLTF = (gltf) => {
+    const mesh = gltf.scene.children[0];
+    mesh.scale.set(0.5, 0.5, 0.5);
+    mesh.material.transparent = true;
+    mesh.material.opacity = .8;
+    mesh.material.receiveShadow = true;
+    mesh.material.castShadow = true;
+    mesh.material.shininess = 10;
+    mesh.material.roughness = 0;
+    mesh.material.metalness = .9;
+    mesh.traverse((node) => {
+        if (node.isLight) {
+            this.state.punctualLights = false;
+        } else if (node.isMesh) {
+            node.material.depthWrite = !node.material.transparent;
+        }
+    });
+
+    return mesh;
+}
+
+let selectedPiece = null;
+
+function pieceClick(e) {
+    if (e.eventObject.material.color.getHex('') !== activeColor) {
+        return;
+    }
+    selectedPiece = e.eventObject;
+    e.eventObject.parent.children.forEach((child) => {
+        if (child.name.startsWith('field-')) {
+            child.material.opacity = 0;
+        }
+    });
+    // selectedPiece.material.color = new THREE.Color(0x0000ff);
+    chess.getPossibleMoves(e.eventObject.position).forEach((move) => {
+        const name = 'field-' + String.fromCharCode(65 + move.y) + (8 - move.x);
+        const field = e.eventObject.parent.getObjectByName(name);
+        console.log(e.eventObject.parent.children, 'children', name, field);
+        field.material.opacity = .5;
+        field.material.color = new THREE.Color(move.capture ? colorWhite : colorBlack);
+        console.log(move, name);
+    });
+}
+
+function fieldClick(e, controls) {
+    console.log('click', e, controls);
+    if (e.eventObject.material.opacity > 0) {
+        const {
+            newPosition,
+            capture,
+            castling
+        } = chess.move(e.eventObject.position);
+        if (newPosition) {
+            const activePlayer = chess.getActivePlayer();
+            if (capture) {
+                const totalCaptured = chess.getCapturedPieces()[activePlayer === 'white' ? 'black' : 'white'].length;
+                const capturedPiece = e.eventObject.parent.children.find((child) => child.position.x === newPosition.x && child.position.z === newPosition.z && !child.name.startsWith('field-'));
+                const capturedPiecePosition = activeColor === colorWhite ? new THREE.Vector3(-5, 0, (-4.25 + totalCaptured * 0.5)) : new THREE.Vector3(5, 0, (4.25 - totalCaptured * 0.5));
+                capturedPiece.position.copy(capturedPiecePosition);
+            }
+            if (castling) {
+                const rook = e.eventObject.parent.children.find((child) => child.position.x === castling.x && child.position.z === castling.z && !child.name.startsWith('field-'));
+                rook.position.copy(new THREE.Vector3(newPosition.x + (newPosition.x > 0 ? -1 : 1), 0, newPosition.z));
+            }
+            selectedPiece.position.copy(newPosition);
+            activeColor = chess.getActivePlayer() === 'white' ? colorWhite : colorBlack;
+            controls.current.rotateTo(chess.getActivePlayer() === 'white' ? 0 : Math.PI, Math.PI / 4, true);
+        }
+        e.eventObject.parent.children.forEach((child) => {
+            if (child.name.startsWith('field-')) {
+                child.material.opacity = 0;
+            }
+        });
+    }
 }
 
 export default App;
---
+++ src/Chess.ts
@@ -0,0 +1,475 @@
+import * as THREE from "three";
+
+export default class Chess {
+    private selectedPiece = [];
+
+    private activePlayer = "white";
+
+    private capturedPieces = {
+        white: [],
+        black: [],
+    }
+
+    private kingNotMoved = {
+        white: true,
+        black: true,
+    }
+
+    private leftRookNotMoved = {
+        white: true,
+        black: true,
+    }
+
+    private rightRookNotMoved = {
+        white: true,
+        black: true,
+    }
+
+    private board = [
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        ["r", "n", "b", "q", "k", "b", "n", "r"]
+    ];
+
+    constructor() {
+        console.log("Chess constructor");
+    }
+
+    public move(position: THREE.Vector3) {
+        const [x, y] = this.getPieceIndex(position);
+        const piece = this.board[this.selectedPiece[0]][this.selectedPiece[1]]
+        const possibleMoves = this.getPossibleMovesForPiece(piece, this.selectedPiece[0], this.selectedPiece[1]);
+        const move = this.getMove(x, y, possibleMoves);
+        console.log("Chess move", move);
+        if (move) {
+            let castlingRook = null
+            if(move.castling) {
+                if(y === 2) {
+                    castlingRook = this.indexToPosition(x, 0);
+                    this.board[move.x][move.y] = this.board[this.selectedPiece[0]][this.selectedPiece[1]];
+                    this.board[this.selectedPiece[0]][this.selectedPiece[1]] = " ";
+                    this.board[move.x][move.y + 1] = this.board[x][0];
+                    this.board[x][0] = " ";
+                }
+                if(y === 6) {
+                    castlingRook = this.indexToPosition(x, 7);
+                    this.board[move.x][move.y] = this.board[this.selectedPiece[0]][this.selectedPiece[1]];
+                    this.board[move.x][move.y] = " ";
+                    this.board[move.x][move.y - 1] = this.board[x][7];
+                    this.board[x][7] = " ";
+                }
+            }
+            if (move.capture) {
+                this.capturedPieces[this.activePlayer].push(this.board[x][y]);
+            }
+            if(piece === "K" || piece === "k") {
+                this.kingNotMoved[this.activePlayer] = false;
+            }
+            if(piece === "R" || piece === "r") {
+                if(this.selectedPiece[1] === 0) {
+                    this.leftRookNotMoved[this.activePlayer] = false;
+                }
+                if(this.selectedPiece[1] === 7) {
+                    this.rightRookNotMoved[this.activePlayer] = false;
+                }
+            }
+            this.board[x][y] = this.board[this.selectedPiece[0]][this.selectedPiece[1]];
+            this.board[this.selectedPiece[0]][this.selectedPiece[1]] = " ";
+            this.selectedPiece = [];
+            this.activePlayer = this.activePlayer === "white" ? "black" : "white";
+            console.log('Chess Board', this.board);
+            return {newPosition: this.indexToPosition(x, y), capture: move.capture, castling: castlingRook};
+        }
+        return null;
+    }
+
+    public getPossibleMoves(position: THREE.Vector3) {
+        const [x, y] = this.getPieceIndex(position);
+        const piece = this.board[x][y];
+        this.selectedPiece = [x, y];
+        return this.getPossibleMovesForPiece(piece, x, y);
+    }
+
+    public getActivePlayer() {
+        return this.activePlayer;
+    }
+
+    public getCapturedPieces() {
+        return this.capturedPieces;
+    }
+
+    private getPieceIndex(position: THREE.Vector3) {
+        return [position.z + 3.5, position.x + 3.5];
+    }
+
+    private getPossibleMovesForPiece(piece: string, x: number, y: number) {
+        console.log("Chess getPossibleMovesForPiece", piece);
+        switch (piece) {
+            case "P":
+            case "p":
+                return this.getPossibleMovesForPawn(x, y, piece);
+            case "r":
+            case "R":
+                return this.getPossibleMovesForRook(x, y, piece);
+            case "n":
+            case "N":
+                return this.getPossibleMovesForKnight(x, y, piece);
+            case "b":
+            case "B":
+                return this.getPossibleMovesForBishop(x, y, piece);
+            case "q":
+            case "Q":
+                return this.getPossibleMovesForQueen(x, y, piece);
+            case "k":
+            case "K":
+                return this.getPossibleMovesForKing(x, y, piece);
+            default:
+                return [];
+        }
+    }
+
+    private getPossibleMovesForPawn(x: number, y: number, symbol: string) {
+        const possibleMoves = [];
+        const regex = symbol === symbol.toLowerCase() ? /[RNBQKP]/ : /[rnbqkp]/;
+
+        // Bewegungen für weiße Bauern
+        if (symbol === "p") {
+            // Ein Schritt nach vorne
+            if (x > 0 && this.board[x - 1][y] === " ") {
+                possibleMoves.push({x: x - 1, y: y, capture: false});
+            }
+
+            // Zwei Schritte nach vorne
+            if (x === 6 && this.board[5][y] === " " && this.board[4][y] === " ") {
+                possibleMoves.push({x: 4, y: y, capture: false});
+            }
+
+            // Schlagbewegungen
+            if (x > 0 && y > 0 && regex.test(this.board[x - 1][y - 1])) {
+                possibleMoves.push({x: x - 1, y: y - 1, capture: true});
+            }
+            if (x > 0 && y < 7 && regex.test(this.board[x - 1][y + 1])) {
+                possibleMoves.push({x: x - 1, y: y + 1, capture: true});
+            }
+        }
+
+        // Bewegungen für schwarze Bauern
+        if (symbol === "P") {
+            // Ein Schritt nach vorne
+            if (x < 7 && this.board[x + 1][y] === " ") {
+                possibleMoves.push({x: x + 1, y: y, capture: false});
+            }
+
+            // Zwei Schritte nach vorne
+            if (x === 1 && this.board[2][y] === " " && this.board[3][y] === " ") {
+                possibleMoves.push({x: 3, y: y, capture: false});
+            }
+
+            // Schlagbewegungen
+            if (x < 7 && y > 0 && regex.test(this.board[x + 1][y - 1])) {
+                possibleMoves.push({x: x + 1, y: y - 1, capture: true});
+            }
+            if (x < 7 && y < 7 && regex.test(this.board[x + 1][y + 1])) {
+                possibleMoves.push({x: x + 1, y: y + 1, capture: true});
+            }
+        }
+
+        console.log("Chess getPossibleMovesForPawn", possibleMoves);
+        return possibleMoves;
+    }
+
+
+    private getPossibleMovesForRook(x: number, y: number, symbol: string) {
+        const currentPosition = this.board[x][y];
+        const possibleMoves: { x: number, y: number, capture: boolean, castling: boolean }[] = [];
+        const regex = symbol === symbol.toLowerCase() ? /[rnbqkp]/ : /[RNBQKP]/;
+        const regexEnemy = symbol === symbol.toLowerCase() ? /[RNBQKP]/ : /[rnbqkp]/;
+
+        // Check for possible moves in horizontal direction
+        for (let i = y - 1; i >= 0; i--) {
+            const newPosition = this.board[x][i];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: x, y: i, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: x, y: i, capture: false, castling: false});
+            }
+        }
+
+        for (let i = y + 1; i < 8; i++) {
+            const newPosition = this.board[x][i];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: x, y: i, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: x, y: i, capture: false, castling: false});
+            }
+        }
+
+        // Check for possible moves in vertical direction
+        for (let i = x - 1; i >= 0; i--) {
+            const newPosition = this.board[i][y];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: i, y: y, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: i, y: y, capture: false, castling: false});
+            }
+        }
+
+        for (let i = x + 1; i < 8; i++) {
+            const newPosition = this.board[i][y];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: i, y: y, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: i, y: y, capture: false, castling: false});
+            }
+        }
+
+        return possibleMoves;
+    }
+
+
+    private getPossibleMovesForKnight(x: number, y: number, symbol: string) {
+        const currentPosition = this.board[x][y];
+        const regex = symbol === symbol.toLowerCase() ? /[rnbqkp]/ : /[RNBQKP]/;
+        const regexEnemy = symbol === symbol.toLowerCase() ? /[RNBQKP]/ : /[rnbqkp]/;
+        const possibleMoves: { x: number, y: number, capture: boolean, castling: boolean }[] = [];
+
+        const moves = [
+            [2, 1],
+            [2, -1],
+            [-2, 1],
+            [-2, -1],
+            [1, 2],
+            [1, -2],
+            [-1, 2],
+            [-1, -2]
+        ];
+
+        for (const [dx, dy] of moves) {
+            const newX = x + dx;
+            const newY = y + dy;
+
+            if (newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+                continue;
+            }
+
+            const newPosition = this.board[newX][newY];
+
+            if (/[kK]/.test(currentPosition) && /[kK]/.test(newPosition)) {
+                continue;
+            }
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                continue;
+            }
+
+            possibleMoves.push({x: newX, y: newY, capture: regexEnemy.test(newPosition), castling: false});
+        }
+
+        return possibleMoves;
+    }
+
+
+    private getPossibleMovesForBishop(x: number, y: number, symbol: string) {
+        const currentPosition = this.board[x][y];
+        const regex = symbol === symbol.toLowerCase() ? /[rnbqkp]/ : /[RNBQKP]/;
+        const regexEnemy = symbol === symbol.toLowerCase() ? /[RNBQKP]/ : /[rnbqkp]/;
+        const possibleMoves: { x: number, y: number, capture: boolean, castling: boolean }[] = [];
+
+        // Check moves along positive diagonal
+        for (let i = 1; i <= Math.min(7 - x, 7 - y); i++) {
+            const newX = x + i;
+            const newY = y + i;
+            const newPosition = this.board[newX][newY];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: newX, y: newY, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: newX, y: newY, capture: false, castling: false});
+            }
+        }
+
+        // Check moves along negative diagonal
+        for (let i = 1; i <= Math.min(x, 7 - y); i++) {
+            const newX = x - i;
+            const newY = y + i;
+            const newPosition = this.board[newX][newY];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: newX, y: newY, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: newX, y: newY, capture: false, castling: false});
+            }
+        }
+
+        // Check moves along negative diagonal
+        for (let i = 1; i <= Math.min(7 - x, y); i++) {
+            const newX = x + i;
+            const newY = y - i;
+            const newPosition = this.board[newX][newY];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: newX, y: newY, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: newX, y: newY, capture: false, castling: false});
+            }
+        }
+
+        // Check moves along negative diagonal
+        for (let i = 1; i <= Math.min(x, y); i++) {
+            const newX = x - i;
+            const newY = y - i;
+            const newPosition = this.board[newX][newY];
+
+            if (regex.test(currentPosition) && regex.test(newPosition)) {
+                break;
+            }
+
+            if (regexEnemy.test(newPosition)) {
+                possibleMoves.push({x: newX, y: newY, capture: true, castling: false});
+                break;
+            } else {
+                possibleMoves.push({x: newX, y: newY, capture: false, castling: false});
+            }
+        }
+
+        return possibleMoves;
+    }
+
+
+    private getPossibleMovesForQueen(x: number, y: number, symbol: string) {
+        return [this.getPossibleMovesForRook(x, y, symbol), this.getPossibleMovesForBishop(x, y, symbol)].flat();
+    }
+
+    private getPossibleMovesForKing(x: number, y: number, symbol: string) {
+        const possibleMoves: { x: number, y: number, capture: boolean, castling: boolean }[] = [];
+        const regex = symbol === symbol.toLowerCase() ? /[rnbqkp]/ : /[RNBQKP]/;
+        const regexEnemy = symbol === symbol.toLowerCase() ? /[RNBQKP]/ : /[rnbqkp]/;
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+
+                const row = x + i;
+                const col = y + j;
+
+                if (row < 0 || row > 7 || col < 0 || col > 7) {
+                    continue;
+                }
+
+                const newPosition = this.board[row][col];
+
+                if (regex.test(newPosition)) {
+                    continue;
+                }
+
+                if (regexEnemy.test(newPosition)) {
+                    possibleMoves.push({x: row, y: col, capture: true, castling: false});
+                } else {
+                    possibleMoves.push({x: row, y: col, capture: false, castling: false});
+                }
+            }
+        }
+
+        if(this.kingNotMoved[this.activePlayer]) {
+            if(this.leftRookNotMoved[this.activePlayer]) {
+                if(this.board[x][y - 1] === " " && this.board[x][y - 2] === " " && this.board[x][y - 3] === " ") {
+                    possibleMoves.push({x: x, y: y - 2, capture: false, castling: true});
+                }
+            }
+            if(this.rightRookNotMoved[this.activePlayer]) {
+                if(this.board[x][y + 1] === " " && this.board[x][y + 2] === " ") {
+                    possibleMoves.push({x: x, y: y + 2, capture: false, castling: true});
+                }
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    private indexToPosition(x: number, y: number) {
+        return new THREE.Vector3(y - 3.5, 0, x - 3.5);
+    }
+
+    private getMove(x: number, y: number, possibleMoves: any): { x: number, y: number, capture: boolean, castling: boolean } | undefined {
+        return possibleMoves.find((move: any) => move.x === x && move.y === y);
+    }
+
+    public isKingInCheck(): boolean {
+        const kingSymbol = this.activePlayer === "white" ? "K" : "k";
+        let kingPosition: [number, number];
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (this.board[i][j] === kingSymbol) {
+                    kingPosition = [i, j];
+                    break;
+                }
+            }
+            if (kingPosition) {
+                break;
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                const piece = this.board[i][j];
+                if (piece && piece.toLowerCase() !== kingSymbol.toLowerCase() && piece.toLowerCase() !== "p" && piece.toLowerCase() !== "k" && piece.toLowerCase() !== " ") {
+                    const possibleMoves = this.getPossibleMovesForPiece(piece, i, j);
+                    for (const move of possibleMoves) {
+                        if (move.x === kingPosition[0] && move.y === kingPosition[1]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
